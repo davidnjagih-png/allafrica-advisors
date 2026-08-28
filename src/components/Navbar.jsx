97cf2a5cb25f4dd32f24ead5f8d74820b4813036
davidnjagih-png <david.njagih@student.moringaschool.com>
2026-08-28 16:38:37 +0300

    navbar changes to add logo, dashboard buton

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
+// src/components/Navbar.jsx
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { FaTachometerAlt } from "react-icons/fa"; // dashboard icon
+//import logo from "../assets/logo.png"; // place your logo file in src/assets
+
+import "../styles/Navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">AllAfrica Advisors</div>
-      <ul>
+      <div className="nav-left">
+        {/* <img src={logo} alt="AllAfrica Advisors Logo" className="nav-logo" /> */}
+        <span className="nav-title">AllAfrica Advisors</span>
+      </div>
+      <ul className="nav-links">
         <li>
           <NavLink to="/" end>
             Home
@@ -25,6 +33,11 @@ function Navbar() {
         </li>
         <li>
           <NavLink to="/contact">Contact</NavLink>
+        </li>
+        <li className="dashboard-link">
+          <NavLink to="/dashboard">
+            <FaTachometerAlt className="dashboard-icon" />
+          </NavLink>
         </li>
       </ul>
     </nav>
